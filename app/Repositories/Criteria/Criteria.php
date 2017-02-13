a8271e4206bf1c07642b21b3de9ca5ad6aec0286
nguyen.thi.thuy.ngan <thuyngan0995@gmail.com>
2017-02-13 16:10:21 +0700

    base repositories

---
+++ app/Repositories/Criteria/Criteria.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Repositories\Criteria;
+
+use App\Repositories\Contracts\RepositoryInterface as Repository;
+
+abstract class Criteria
+{
+
+    /**
+     * @param $model
+     * @param Repository $repository
+     * @return mixed
+     */
+    public abstract function apply($model, Repository $repository);
+}
